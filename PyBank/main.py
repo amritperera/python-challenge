0d398b23065422df10d2423635235cbfa7729d43
AmritPerera <amrit.perera@yahoo.com>
2018-11-28 17:35:18 -0600

    Update main.py
    close to finished part 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,10 +13,33 @@ for x in range(total_months):
 #Average Calculation
 average_change = (bd.iat[total_months-1,1] - bd.iat[0,1]) / (len(bd.index)-1)
 
+#Greatest Increase Calculation
+check = 0
+for x in range(total_months-1):
+	g_incTest = bd.iat[x+1,1] - bd.iat[x,1]
+	if g_incTest > check:
+		date = bd.iat[x+1,0]
+		g_inc = g_incTest
+		check = g_incTest
+
+#Greatest Decrease Calculation
+check = 0
+for x in range(total_months-1):
+	g_decTest = bd.iat[x+1,1] - bd.iat[x,1]
+	if g_decTest < check:
+		date2 = bd.iat[x+1,0]
+		g_dec = g_decTest
+		check = g_decTest
+
+#Format Some Variables
+average_change = '${:,.2f}'.format(average_change)
+#Output
 print("")
 print("Financial Analysis")
 print("----------------------------")
 
 print(f"Total Months: {total_months}")
 print(f"Total: ${running_total}")
-print(f"Average Change: ${average_change}")
+print(f"Average Change: {average_change}")
+print(f"Greatest Increase in Profits: {date} (${g_inc})")
+print(f"Greatest Decrease in Profits: {date2} (${g_dec})")
