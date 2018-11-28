9842a26db8205b652d26e88701e664bcb66dd29a
AmritPerera <amrit.perera@yahoo.com>
2018-11-28 16:56:50 -0600

    bank py changes - totals and average

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,22 @@
 import pandas
 
-df =  pandas.read_csv('budget_data.csv')
-print(df)
+bd =  pandas.read_csv('budget_data.csv')
 
-print("test 2")
+total_months = (len(bd.index))
+
+running_total = 0
+
+#Total Net Profit/Loss Calculation
+for x in range(total_months):
+	running_total = bd.iat[x,1] + running_total
+
+#Average Calculation
+average_change = (bd.iat[total_months-1,1] - bd.iat[0,1]) / (len(bd.index)-1)
+
+print("")
+print("Financial Analysis")
+print("----------------------------")
+
+print(f"Total Months: {total_months}")
+print(f"Total: ${running_total}")
+print(f"Average Change: ${average_change}")
